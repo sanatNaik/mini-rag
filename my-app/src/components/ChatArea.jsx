44aaa7177f7f1f82e00897a5d1ba51a6801724bd
sanatNaik <sanatnaik2005@gmail.com>
2025-09-05 23:29:11 +0530

    changes to frontend4

--- my-app/src/components/ChatArea.jsx
+++ my-app/src/components/ChatArea.jsx
@@ -13,7 +13,7 @@ const ChatArea = () => {
   return (
     <div className='w-[90%] flex flex-col justify-center items-center bg-black text-white '>
         <div className='flex w-full h-full justify-around items-center'>
-            <textarea value={query} className='w-[85%] h-[90%] p-2 overflow-y-scroll rounded-lg' name="query" id="" placeholder='Enter your question here..' onChange={e => setQuery(e.target.value)}></textarea>
+            <textarea value={query} className='w-[85%] h-[90%] p-2 overflow-y-scroll rounded-lg text-black' name="query" id="" placeholder='Enter your question here..' onChange={e => setQuery(e.target.value)}></textarea>
             <button className='flex w-[10%] h-[70%] bg-white text-black justify-center items-center rounded-lg' onClick={(e) => handleSubmit(e)}>
               Submit
             </button>
